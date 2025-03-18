9aa41f8ed4c02b6995a4679f3c3e16c0241f10d8
Vy Duc Tan <vdt5snet@gmail.com>
2025-03-18 08:47:46 +0000

    feat: orgs, teams, projects

---
+++ README.md
@@ -0,0 +1,29 @@
+# Tree component
+
+## Dnd Kit
+
+Nov 23, 2024
+
+<https://github.com/clauderic/dnd-kit/commit/f629ec6a9c3c25b749561fac31741046d96c28dc>
+<https://github.com/clauderic/dnd-kit/blob/master/stories/3%20-%20Examples/Tree/SortableTree.tsx>
+<https://master--5fc05e08a4a65d0021ae0bf2.chromatic.com/?path=/story/examples-tree-sortable--all-features>
+
+## dnd-kit-sortable-tree
+
+Nov 19, 2023, 5:29 PM GMT+7
+<https://github.com/Shaddix/dnd-kit-sortable-tree/blob/master/src/SortableTree.tsx>
+<https://shaddix.github.io/dnd-kit-sortable-tree/?path=/story/simple--simple>
+
+## Antd
+
+Jan 27, 2025
+<https://github.com/ant-design/ant-design/tree/master/components/tree>
+
+Jan 7, 2025
+<https://github.com/react-component/tree>
+
+## Shadcn
+
+Jan 17, 2025
+
+<https://github.com/MrLightful/shadcn-tree-view/commit/5b7cfc6ac76e652c0a892d5d27ab26dc0878df6f>
---
+++ src/utils.ts
@@ -0,0 +1,313 @@
+import type { UniqueIdentifier } from "@dnd-kit/core";
+import { arrayMove } from "@dnd-kit/sortable";
+
+import type { AnyObject } from "@acme/ui";
+
+import type { TreeProps } from "./tree";
+import type { FlattenedNode, TreeDataNode } from "./types";
+
+export const iOS =
+  navigator.userAgent.includes("iPad") ||
+  navigator.userAgent.includes("iPhone") ||
+  navigator.userAgent.includes("iPod");
+
+function getDragDepth(offset: number, indentationWidth: number) {
+  return Math.round(offset / indentationWidth);
+}
+
+export function getProjection<TRecord extends AnyObject>(
+  items: FlattenedNode<TRecord>[],
+  activeId: UniqueIdentifier | undefined,
+  overId: UniqueIdentifier | undefined,
+  dragOffset: number,
+  indentationWidth: number,
+  allowDrop?: TreeProps<TRecord>["allowDrop"],
+  allowDepthOnDrag?: TreeProps<TRecord>["allowDepthOnDrag"],
+): {
+  depth: number;
+  maxDepth: number;
+  minDepth: number;
+  parentId: UniqueIdentifier | null;
+  parent: FlattenedNode<TRecord> | null;
+  isLeaf: boolean;
+} | null {
+  if (!activeId || !overId) return null;
+
+  const overItemIndex = items.findIndex(({ key }) => key === overId);
+  const overItem = items[overItemIndex]!;
+  const activeItemIndex = items.findIndex(({ key }) => key === activeId);
+  const activeItem = items[activeItemIndex]!;
+  const newItems = arrayMove(items, activeItemIndex, overItemIndex);
+  const previousItem = newItems[overItemIndex - 1];
+  const nextItem = newItems[overItemIndex + 1];
+  const dragDepth = getDragDepth(dragOffset, indentationWidth);
+  const projectedDepth = activeItem.depth + dragDepth;
+  // const maxDepth = getMaxDepth({
+  //   previousItem,
+  // });
+  // const minDepth = getMinDepth({ nextItem });
+  let depth = projectedDepth;
+
+  const directParent = findParentWithDepth(depth - 1, previousItem);
+  const parent = findParentWhichCanHaveChildren(directParent);
+
+  if (parent === undefined) return null;
+  if (parent?.isLeaf) return null;
+
+  const maxDepth = (parent?.depth ?? -1) + 1;
+  const minDepth = nextItem?.depth ?? 0;
+
+  const calculatedDepth = allowDepthOnDrag?.({
+    activeItem,
+    overItem,
+    parent,
+    flattenedItems: items,
+  });
+
+  if (calculatedDepth === null || calculatedDepth === undefined) {
+    // if (minDepth > maxDepth) return null;
+    // condition first => fix item should not go to back position and not go root
+    if (minDepth > maxDepth) {
+      depth = minDepth;
+    } else if (depth >= maxDepth) {
+      depth = maxDepth;
+    } else if (depth < minDepth) {
+      depth = minDepth;
+    }
+  } else {
+    // Use custom depth
+    depth = calculatedDepth;
+  }
+
+  const isLeaf = (nextItem?.depth ?? -1) < depth;
+
+  // const parent = getParent();
+  const parentId = getParentId();
+  // Validate drop with allowDrop callback
+  if (allowDrop) {
+    const isAllowDrop = allowDrop({
+      activeItem,
+      overItem,
+      parentId,
+      parent,
+      flattenedItems: items,
+    });
+    if (!isAllowDrop) return null;
+  }
+
+  return { depth, maxDepth, minDepth, parentId, parent, isLeaf };
+
+  function findParentWithDepth(
+    depth: number,
+    previousItem: FlattenedNode<TRecord> | undefined,
+  ) {
+    if (!previousItem) return null;
+    while (depth < previousItem.depth) {
+      if (previousItem.parent === null) return null;
+      previousItem = previousItem.parent;
+    }
+    return previousItem;
+  }
+  function findParentWhichCanHaveChildren(
+    parent: FlattenedNode<TRecord> | null,
+  ): FlattenedNode<TRecord> | null | undefined {
+    if (!parent) {
+      return parent;
+    }
+    const canHaveChildren = parent.isLeaf ? false : true;
+    if (canHaveChildren === false)
+      return findParentWhichCanHaveChildren(parent.parent);
+    return parent;
+  }
+
+  function getParentId() {
+    if (depth === 0 || !previousItem) {
+      return null;
+    }
+
+    if (depth === previousItem.depth) {
+      return previousItem.parentId;
+    }
+
+    if (depth > previousItem.depth) {
+      return previousItem.key;
+    }
+
+    const newParent = newItems
+      .slice(0, overItemIndex)
+      .reverse()
+      .find((item) => item.depth === depth)?.parentId;
+
+    return newParent ?? null;
+  }
+}
+
+// function getMaxDepth({
+//   previousItem,
+// }: {
+//   previousItem: FlattenedNode | undefined;
+// }) {
+//   if (previousItem) {
+//     return previousItem.depth + 1;
+//   }
+
+//   return 0;
+// }
+
+// function getMinDepth({ nextItem }: { nextItem: FlattenedNode | undefined }) {
+//   if (nextItem) {
+//     return nextItem.depth;
+//   }
+
+//   return 0;
+// }
+
+function flatten<TRecord extends AnyObject>(
+  items: TreeDataNode[],
+  parentId: UniqueIdentifier | null = null,
+  parent: FlattenedNode<TRecord> | null = null,
+  depth = 0,
+): FlattenedNode<TRecord>[] {
+  const result: FlattenedNode<TRecord>[] = [];
+
+  for (const [index, item] of items.entries()) {
+    const flattenedItem: FlattenedNode = {
+      // key: "1",
+      // title: "2",
+      ...item,
+      parentId,
+      depth,
+      index,
+      // isLeaf: items.length === index + 1,
+      parent,
+    };
+    result.push(flattenedItem);
+
+    if (item.children && item.children.length > 0) {
+      result.push(
+        ...flatten(item.children, flattenedItem.key, flattenedItem, depth + 1),
+      );
+    }
+  }
+  return result;
+}
+
+export function flattenTree(items: TreeDataNode[]): FlattenedNode[] {
+  return flatten(items);
+}
+
+export function buildTree(flattenedItems: FlattenedNode[]): TreeDataNode[] {
+  const root: TreeDataNode = { key: "root", title: "", children: [] };
+  const nodes: Record<string, TreeDataNode> = { [root.key]: root };
+  const items = flattenedItems.map((item) => ({ ...item, children: [] }));
+
+  for (const item of items) {
+    const { key, title, children } = item;
+    const parentId = item.parentId ?? root.key;
+    const parent = nodes[parentId] ?? findItem(items, parentId);
+
+    nodes[key] = { key, title, children };
+    parent?.children?.push(item);
+  }
+
+  return root.children ?? [];
+}
+
+export function findItem(items: TreeDataNode[], itemId: UniqueIdentifier) {
+  return items.find(({ key }) => key === itemId);
+}
+
+export function findItemDeep(
+  items: TreeDataNode[],
+  itemId: UniqueIdentifier,
+): TreeDataNode | undefined {
+  for (const item of items) {
+    const { key, children } = item;
+
+    if (key === itemId) {
+      return item;
+    }
+
+    if (children && children.length > 0) {
+      const child = findItemDeep(children, itemId);
+
+      if (child) {
+        return child;
+      }
+    }
+  }
+
+  return undefined;
+}
+
+export function removeItem(items: TreeDataNode[], id: UniqueIdentifier) {
+  const newItems = [];
+
+  for (const item of items) {
+    if (item.key === id) {
+      continue;
+    }
+
+    if (item.children && item.children.length > 0) {
+      item.children = removeItem(item.children, id);
+    }
+
+    newItems.push(item);
+  }
+
+  return newItems;
+}
+
+export function setProperty<T extends keyof TreeDataNode>(
+  items: TreeDataNode[],
+  id: UniqueIdentifier,
+  property: T,
+  setter: (value: TreeDataNode[T]) => TreeDataNode[T],
+) {
+  for (const item of items) {
+    if (item.key === id) {
+      item[property] = setter(item[property]);
+      continue;
+    }
+
+    if (item.children && item.children.length > 0) {
+      item.children = setProperty(item.children, id, property, setter);
+    }
+  }
+
+  return [...items];
+}
+
+function countChildren(items: TreeDataNode[], count = 0): number {
+  for (const { children } of items) {
+    if (children && children.length > 0) {
+      return countChildren(children, count + 1);
+    }
+    count = count + 1;
+  }
+  return count;
+}
+
+export function getChildCount(items: TreeDataNode[], id: UniqueIdentifier) {
+  const item = findItemDeep(items, id);
+
+  return item?.children ? countChildren(item.children) : 0;
+}
+
+export function removeChildrenOf<T extends AnyObject>(
+  items: FlattenedNode<T>[],
+  ids: UniqueIdentifier[],
+) {
+  const excludeParentIds = [...ids];
+
+  return items.filter((item) => {
+    if (item.parentId && excludeParentIds.includes(item.parentId)) {
+      if (item.children?.length) {
+        excludeParentIds.push(item.key);
+      }
+      return false;
+    }
+
+    return true;
+  });
+}
